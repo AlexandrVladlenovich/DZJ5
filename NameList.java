421f043e12d930e6f8e920c126e0016e5941153c
AlexandrVladlenovich <110374871+AlexandrVladlenovich@users.noreply.github.com>
2022-12-06 21:43:56 +0500

    NameList.java

---
+++ NameList.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+
+public class NameList {
+    public static void main(String[] args) {
+        List<String> spisok = List.of("Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина", "Анна Крутова",
+            "Иван Юрин", "Петр Лыков", "Павел Чернов", "Петр Чернышов", "Мария Федорова", "Марина Светлова", "Мария Савина", "Мария Рыкова",
+            "Марина Лугова", "Анна Владимирова", "Иван Мечников", "Петр Петин", "Иван Ежов");
+
+        Map<String, Integer> spisokImen = new HashMap<String, Integer>(); 
+        for (String strElem : spisok) { 
+            String[] tempArr = strElem.split(" ");
+            if (spisokImen.containsKey(tempArr[0])) {
+                spisokImen.put(tempArr[0], spisokImen.get(tempArr[0]) + 1);
+            } else {
+                spisokImen.put(tempArr[0], 1);
+            }
+        }
+
+        for (var item : spisokImen.entrySet()) { // выводим какое имя сколько раз повторялось
+            System.out.printf("Имя: %s, встречается %d раз(а)\n", item.getKey(), item.getValue());
+        }
+
+        List<String> tempListName = new ArrayList<String>(); // создаем временный список с именами по порядку возрастания
+        int tempNumber = 1;
+        while(tempNumber < spisok.size()) {
+            for (Map.Entry<String, Integer> item : spisokImen.entrySet()) {
+                if (item.getValue() == tempNumber) tempListName.add(0, item.getKey());
+            }
+            tempNumber++;
+        }
+        System.out.println(tempListName);
+
+        for (String str : tempListName) { 
+            for (String strElem : spisok) {
+                String[] tempArr = strElem.split(" ");
+                if (str.equals(tempArr[0])) System.out.println(strElem);
+            }
+        }
+    }
+}
